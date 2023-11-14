91b108d3c6cc5c0a51b0004933af69b1b3f81d4b
wssalvador <110932141+wssalvador@users.noreply.github.com>
2023-11-14 17:00:54 +0000

    Delete utils.py
    Belonged to the previous version of BEMSCA, has since been updated.

--- utils.py
+++
@@ -1,9 +0,0 @@
-import os, shutil
-
-def clear_folders():
-    folders = ['simul', 'workflows', 'results', 'studies']
-    for folder in folders:
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
